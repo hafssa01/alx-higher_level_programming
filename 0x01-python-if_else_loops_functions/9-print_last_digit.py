6e83db1164e9e98bac6a56a719abcc45297e473f
hafssa01 <hafssazaferr@gmail.com>
2023-11-28 11:32:08 -0800

    Conditions_fuctions

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    ld = number % 10
-    return ld
+    if number < 0:
+        number = -number
+    last_digit = number % 10
+    print("{}".format(last_digit), end='')
+    return (last_digit)
