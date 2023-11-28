6e60b334804baf2c2bb30bc35794c1212961a89f
hafssa01 <hafssazaferr@gmail.com>
2023-11-28 11:12:07 -0800

    Conditions_fuctions

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     for itrtr in str:
         tmp = itrtr
-        if prd(tmp) >= 97 and ord(tmp) <= 122:
+        if ord(tmp) >= 97 and ord(tmp) <= 122:
             tmp = chr(ord(itrtr) - 32)
         print("{}".format(tmp), end='')
     print()
