a0f69ae7e8b2738b5edfff5b7ea9f3cc1ec9b928
hafssa01 <hafssazaferr@gmail.com>
2023-11-28 10:49:47 -0800

    Conditions_fuctions

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(ch):
-        if ord(ch) >= ord('a') and ord(ch) <= ord('z'):
+    if ord(ch) >= ord('a') and ord(ch) <= ord('z'):
             return True
         else:
             return False
