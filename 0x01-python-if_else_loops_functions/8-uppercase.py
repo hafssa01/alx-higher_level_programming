568961672bbab4e8fe57a548d07ff642aa1fa067
hafssa01 <hafssazaferr@gmail.com>
2023-11-28 10:37:59 -0800

    Python_tasks

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def islower(ch):
+        if ord(ch) >= ord('a') and ord(ch) <= ord('z'):
+            return True
+        else:
+            return False
+
+def uppercase(str):
+    for ch in str:
+        print("{:c}"
+                .format(ord(ch) if not islower(ch) else ord(ch) - 32),
+                end="")
+        print("")
