7b3c06b362e196b33c1e14b7814d9b4186542ce5
hafssa01 <hafssazaferr@gmail.com>
2023-11-28 06:46:58 -0800

    Python_tasks

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for ft in range(10):
+    for sd in range(10):
+        if ft < sd:
+            print("{:d}{:d}".format(ft, sd),
+                    end="\n" if ft == 8 and sd == 9 else ", ")
