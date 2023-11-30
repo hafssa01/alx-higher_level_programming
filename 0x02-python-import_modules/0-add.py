f6303257c9873f07798c56d545d7ae23dfad2540
hafssa01 <hafssazaferr@gmail.com>
2023-11-30 08:32:59 -0800

    This_is_import_modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-def add(a, b):
-    res = (a + b)
-    print(f"{a} + {b} = {res}")
-    return (a + b)
-
-add(1, 2)
+if __name__ = "__main__":
+    from add import add
+    a = 1
+    b = 2
+    print(f"{a} + {b} = {add(a, b)}")
