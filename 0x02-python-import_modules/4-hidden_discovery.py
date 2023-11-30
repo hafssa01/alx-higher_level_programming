975684d17b8a40c7f456d2e469f59a36717ccdf5
hafssa01 <hafssazaferr@gmail.com>
2023-11-30 12:04:52 -0800

    This_is_import_modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import hidden_4
 
-    for n in dir(hidden_4)
-    if n[:2] != "__":
-        print(n)
+    for n in dir(hidden_4):
+        if n[:2] != "__":
+            print(n)
