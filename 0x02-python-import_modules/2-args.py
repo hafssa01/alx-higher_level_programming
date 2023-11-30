70bebe510fb1decee184831828f57fba3c78a49d
hafssa01 <hafssazaferr@gmail.com>
2023-11-30 10:56:59 -0800

    This_is_import_modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    cnt = len(sys.argv) - 1
+    if cnt == 0:
+        print("0 argument.")
+    elif cnt == 1:
+        print("1 argument.")
+    else:
+        print("{} argument:".format(cnt))
+    for n in range(cnt):
+        print("{}: {}".format(n + 1, sys.argv[n + 1])) 
