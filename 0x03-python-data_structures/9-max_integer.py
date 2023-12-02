438cba61839a32ebd2c436247bb24060787e9916
hafssa01 <hafssazaferr@gmail.com>
2023-12-02 05:02:09 -0800

    Data_structure

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,11 +2,6 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-
-    max_value = my_list[0]
-
-    for num in my_list:
-        if num > max_value:
-            max_value = num
-
-            return max_value
+    list_copy = my_list.copy()
+    list_copy.sort()
+    return list_copy[-1]
