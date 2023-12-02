bd593bb9138209da616c5141becff1fe78435c4d
hafssa01 <hafssazaferr@gmail.com>
2023-12-02 03:15:42 -0800

    Data_structure

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,5 +4,5 @@ def new_in_list(my_list, idx, element):
         return my_list
 
     my_list_copy = my_list.copy()
-    my_list[idx] = element
+    my_list_copy[idx] = element
     return my_list_copy
