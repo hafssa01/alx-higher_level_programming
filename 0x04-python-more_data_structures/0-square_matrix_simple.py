7631c3f9c509738eb02d0b3c4c8594a09a176ef0
hafssa01 <hafssazaferr@gmail.com>
2023-12-06 11:39:04 -0800

    more_data_structure

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,13 @@
 #!/user/bin/python3
 def square_matrix_simple(matrix=[]):
-    return list(map(lambda sbmt: list(map(lambda el: el**2, sbmt)), matrix))
+    result_matrix = []
+
+    for row in matrix:
+        result_row = []
+
+        for element in row:
+            result_row.append(element ** 2)
+
+            result_matrix.append(result_row)
+
+    return result_matrix
