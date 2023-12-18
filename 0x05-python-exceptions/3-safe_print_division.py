4f32f8805b2995c89d7390c82d3ee29c887462e1
hafssa01 <hafssazaferr@gmail.com>
2023-12-18 06:36:33 -0800

    Exceptions

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    try:
+        inside_result = a / b
+    except ZeroDivisionError:
+        inside_result = None
+    finally:
+        print("Inside result: {}".format(inside_result))
+        return inside_result
