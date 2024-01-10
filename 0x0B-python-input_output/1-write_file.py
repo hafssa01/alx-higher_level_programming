f155f0e70797f1a4e904b8a81562522e66b30a54
hafssa01 <hafssazaferr@gmail.com>
2024-01-10 02:53:26 -0800

    O/I

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Define write_file function"""
+"""Define write_file function with two arguments"""
 
 
 
 def write_file(filename="", text=""):
-    """Writes in file"""
+    """Writes in filename"""
     with open(filename, "w", encoding='utf-8') as f:
         return f.write(text)
