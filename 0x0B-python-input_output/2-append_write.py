741142d29f74425b1c99b05a5cbaf9eb0776ff44
hafssa01 <hafssazaferr@gmail.com>
2024-01-10 04:39:38 -0800

    O/I

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,5 +4,5 @@
 
 def append_write(filename="", text=""):
     """Appends in filename with utf8"""
-    with open(filename, "a", encoding='utf-8') as f
-    return f.write(text)
+    with open(filename, "a", encoding='utf-8') as f:
+        return f.write(text)
