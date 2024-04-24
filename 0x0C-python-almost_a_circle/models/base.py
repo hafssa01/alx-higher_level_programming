2aeb1144778b834ea56c1b9b76af33339eeaff02
hafssa01 <hafssazaferr@gmail.com>
2024-04-24 15:26:56 +0000

    tests

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''Module for Base class.'''
-
+from json import dumps, loads
 
 class Base:
 	'''A representation of the base of our OOP hierarchy.'''
@@ -14,3 +14,11 @@ class Base:
 		else:
 			Base.__nb_objects += 1
 			self.id = Base.__nb_objects
+
+	@staticmethode
+	def to_json_string(list_dictionaries):
+		'''jsonifies a dictionary so it's quite righty and longer'''
+		if list_dictionaries is None or not list_dictionaries:
+			return "[]"
+		else:
+			return dumps(list_dictionaries)
