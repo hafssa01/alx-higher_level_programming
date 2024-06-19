b23b345f592cd5e3f192e6da1b20c6ecfcaaec44
hafssa01 <hafssazaferr@gmail.com>
2024-06-19 19:25:16 +0000

    javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if ((w > 0) || (h > 0)) {
+    if ((w > 0) && (h > 0)) {
       this.width = w;
       this.height = h;
   
@@ -11,4 +11,4 @@ class Rectangle {
 }
 
 
-module.exports = Rectangl;
+module.exports = Rectangle;
