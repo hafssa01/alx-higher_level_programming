507685e5dbd48a89679c079eac686e52cd7617fc
hafssa01 <hafssazaferr@gmail.com>
2024-07-04 19:24:18 +0000

    pyhton_network#1

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a request to that URL,
-# And displays the size of the body of the response
-curl -o /dev/null -s -w %{size_download} "$1"
+# Bash script takes in a URL,sends a request to it,and displays the size of the body of the response
+curl -s "$1" | wc -c
