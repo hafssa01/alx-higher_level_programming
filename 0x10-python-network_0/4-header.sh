9f9ec3cedc7ff3038a4f45e2dd11a0c050d4c035
hafssa01 <hafssazaferr@gmail.com>
2024-07-04 22:35:49 +0000

    python-network#5

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#Sends a GET request to the URL, and displays the body of the response
+# Sends a GET request to the URL, and displays the body of the response
 crul -sH "X-School-User-Id: 98" "$1"
