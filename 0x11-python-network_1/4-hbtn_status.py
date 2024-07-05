fbbb6f63f040507066728e4628a2777ed234c082
hafssa01 <hafssazaferr@gmail.com>
2024-07-05 22:53:32 +0000

    python-network#4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Python script that fetches https://alx-intranet.hbtn.io/status"""
+if __name__ == '__main__':
+	import requests
+	
+	req = requests.get('https://intranet.hbtn.io/status')
+
+	print('Body response:')
+	print('\t- type: {}'.format(type(req.text)))
+	print('\t- content: {}'.format(req.text))
