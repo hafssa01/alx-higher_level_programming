403f924b68eceaba2177eba07f2a38aca8660d27
hafssa01 <hafssazaferr@gmail.com>
2024-07-06 10:00:48 +0000

    python-network#_requests

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-""" Python script that fetches https://alx-intranet.hbtn.io/status"""
-if __name__ == '__main__':
-	import requests
-	
-	req = requests.get('https://intranet.hbtn.io/status')
+"""Fetches https://intranet.hbtn.io/status"""
 
-	print('Body response:')
-	print('\t- type: {}'.format(type(req.text)))
-	print('\t- content: {}'.format(req.text))
+
+if __name__ == "__main__":
+    import requests
+
+    req = requests.get('https://intranet.hbtn.io/status')
+
+    print('Body response:')
+    print('\t- type: {}'.format(type(req.text)))
+    print('\t- content: {}'.format(req.text))
--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL, sends a request to the URL and displays the value of the variable X-Request-Id in the response header"""
-if __name__ == '__main__':
-	import requests
-	import sys
-	
-	req = requests.get(sys.argv[1])
-	print(req.header.get('X-Request-Id'))
+"""Takes in a URL, sends a request to the URL and displays the value of the
+X-Request-Id variable found in the header of the response"""
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    req = requests.get(sys.argv[1])
+    print(req.headers.get('X-Request-Id'))
