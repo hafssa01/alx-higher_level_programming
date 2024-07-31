5f8b34eb5bb5b252c1f1ce825386942bb5e7841f
hafssa01 <hafssazaferr@gmail.com>
2024-07-31 15:42:08 +0000

    javascript_web_scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-const file_path = process.argv[2];
 
+const file_path = process.argv[2];
 fs.readFile(file_path, 'utf8', (err, data) => {
 	if (err) {
 		console.log(err);
